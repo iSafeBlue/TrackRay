3af3ec0d24db612e48a04e7f83659f53b957808d
浅蓝 <blue@ixsec.org>
2019-01-28 16:25:03 +0800

    溯光2.0

--- base/src/main/java/com/trackray/base/annotation/Param.java
+++
@@ -1,8 +0,0 @@
-package com.trackray.base.annotation;
-
-public @interface Param {
-    String key () default "";
-    String defaultValue() default "";
-    String desc() default "";
-
-}
--- web/src/main/java/com/trackray/web/query/Query.java
+++
@@ -1,5 +0,0 @@
-package com.trackray.web.query;
-
-public class Query {
-
-}
