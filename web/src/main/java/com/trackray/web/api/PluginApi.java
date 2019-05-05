254275e0d867e9728f1356439920a34ebf716187
浅蓝 <blue@ixsec.org>
2019-05-05 17:31:17 +0800

    更新插件接口注释

--- web/src/main/java/com/trackray/web/api/PluginApi.java
+++ web/src/main/java/com/trackray/web/api/PluginApi.java
@@ -6,7 +6,6 @@ import net.sf.json.JSONArray;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,6 +21,10 @@ public class PluginApi {
     @Autowired
     private PluginService pluginService;
 
+    /**
+     * 列出所有插件
+     * @return
+     */
     @ResponseBody
     @RequestMapping(value = "list" )
     public ResultCode list(){
@@ -29,6 +32,13 @@ public class PluginApi {
         return ResultCode.getInstance(200,"正常",plugins);
     }
 
+    /**
+     * 调用接口式插件
+     * @param map   请求参数
+     * @param request   servlet请求对象
+     * @param response  servlet响应对象
+     * @throws IOException
+     */
     @RequestMapping(value = "use")
     public void use(@RequestParam Map<String,String> map , HttpServletRequest request ,HttpServletResponse response) throws IOException {
         response.setContentType("application/json;charset=utf-8");
@@ -41,7 +51,18 @@ public class PluginApi {
         }
     }
 
-    @RequestMapping(value = "/fetch/{plugin}/{function}")
+    /**
+     * MVC类插件调用
+     * @param plugin 插件名
+     * @param function  功能名
+     * @param map   请求参数
+     * @param model Model和View对象
+     * @param request   servlet请求对象
+     * @param response  servlet响应对象
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/mvc/{plugin}/{function}")
     public ModelAndView fetch(
             @PathVariable String plugin,
             @PathVariable(required = false) String function,
@@ -67,7 +88,15 @@ public class PluginApi {
         }
         return ResultCode.ERROR("参数不可为空");
     }
-*/
+    */
+
+    /**
+     * 多线程插件结果调用接口
+     * @param task  taskid
+     * @param request   servlet请求
+     * @param response  servlet响应
+     * @throws IOException
+     */
     @ResponseBody
     @RequestMapping(value = "result")
     public void result(String task , HttpServletRequest request , HttpServletResponse response) throws IOException {
@@ -85,7 +114,11 @@ public class PluginApi {
     }
 
 
-
+    /**
+     * 停止多线程插件接口
+     * @param task  taskid
+     * @return
+     */
     @ResponseBody
     @RequestMapping(value = "stop")
     public ResultCode stop(String task){
--- web/src/main/java/com/trackray/web/query/TaskQuery.java
+++ web/src/main/java/com/trackray/web/query/TaskQuery.java
@@ -6,14 +6,25 @@ import lombok.Data;
 @Data
 public class TaskQuery {
 
-    private String name = "";
-    private String target = "";
-    private int thread = 0;
-    private int spiderMax = 0 ;
-    private int spiderDeep = 0;
-    private int timeMax = 0;
-    private String cookie = "";
-    private String proxy = "";
-    private Rule rule = new Rule();
+    private String name = "";   //任务名
+
+
+    private String target = ""; //任务目标 可以是 url 可以是域名 可以是主机名
+
+    private int thread = 0;     //最大线程数
+
+    private int spiderMax = 0 ; //最大爬虫页数
+
+    private int spiderDeep = 0; //最大爬虫深度
+
+    private int timeMax = 0;    //任务最长时间 单位：分钟
+
+    private String cookie = ""; //cookie
+
+    private String headers = "";//额外请求头
+
+    private String proxy = "";  //代理池
+
+    private Rule rule = new Rule(); //扫描开关
 
 }
