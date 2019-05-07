dfb6e95424ad4609dc32f8cb636a8c7275b13d38
浅蓝 <blue@ixsec.org>
2019-05-07 18:04:44 +0800

    删除旧版代码

--- base/src/main/java/com/trackray/base/controller/DispatchController.java
+++ base/src/main/java/com/trackray/base/controller/DispatchController.java
@@ -1,13 +1,10 @@
 package com.trackray.base.controller;
 
-import com.trackray.base.attack.FuzzDomain;
-import com.trackray.base.attack.SQLMap;
 import com.trackray.base.bean.Constant;
 import com.trackray.base.bean.ResultCode;
 import com.trackray.base.bean.Task;
 import com.trackray.base.exploit.AbstractExploit;
 import com.trackray.base.httpclient.CrawlerPage;
-import com.trackray.base.httpclient.HttpClient;
 import com.trackray.base.plugin.AbstractPlugin;
 import com.trackray.base.utils.ReUtils;
 import com.trackray.base.utils.SysLog;
@@ -30,70 +27,12 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class DispatchController {
 
     @Autowired
-    private DomainController domainController;
-    @Autowired
-    private IpController ipController;
-    @Autowired
-    private CrawlerController crawlerController;
-    @Autowired
     private WebApplicationContext webApplicationConnect;
-    public void init(Task task){
 
-
-        String targetStr = task.getTargetStr();
-        Task.Target target = new Task.Target();
-        if(ReUtils.isIp(targetStr)){
-            target.ip = targetStr;
-            target.type = Constant.IP_TYPE;
-        }else{
-            target.url = targetStr;
-            target.type = Constant.URL_TYPE;
-        }
-        task.setTarget(target);
-        /*String md5URL = MD5.getMD5Code(task.getTargetStr());
-        task.setTaskMD5(md5URL);*/
-        SysLog.info("任务初始化 type="+target.type);
-    }
-    public ResultCode dispatch(Task task){
-        if (task.getTarget().type == Constant.IP_TYPE){
-            SysLog.info("目标是IP类型，准备扫描中...");
-            return ipController.scan(task);
-        }
-        if (task.getTarget().type == Constant.URL_TYPE){
-            SysLog.info("目标是域名类型，准备扫描中...");
-            if (task.getRule().childdomain && task.getTarget().type == Constant.URL_TYPE){
-                SysLog.info("正在扫描子域名...");
-                FuzzDomain.fuzz(task);
-            }
-            return domainController.scan(task);
-        }
-        return ResultCode.ERROR;
-    }
-
-
-    public void crawler(Task task, String target , ThreadPoolExecutor exec) {
-            CrawlerPage crawler = new CrawlerPage();
-            crawler.setTask(task);
-            //目前只对目标站进行爬虫
-            crawler.getRequest().setUrl(target);
-            crawler.setBase(target);
-
-            Runnable runable = new Runnable() {
-                @Override
-                public void run() {
-                    crawlerController.crawler(crawler);
-                    Runnable sqlmap =(Runnable) crawler.getTempData().remove("sqlmap");
-
-                    exec.execute(sqlmap);
-                }
-            };
-            exec.execute(runable);
-
-
-    }
     public WebApplicationContext getAppContext(){
         return webApplicationConnect;
     }
+    @Deprecated
     public void attack(Task task, ExecutorService exec) {
         WebApplicationContext context = getAppContext();
         Map<String, AbstractExploit> beans = context.getBeansOfType(AbstractExploit.class);
--- base/src/main/java/com/trackray/base/service/SystemService.java
+++
@@ -1,8 +0,0 @@
-package com.trackray.base.service;
-
-import com.trackray.base.bean.ResultCode;
-import com.trackray.base.bean.Task;
-
-public interface SystemService {
-    ResultCode scan(Task task);
-}
