23d71381788a9e0eb2c237e53a346d22f1d0efd3
浅蓝 <blue@ixsec.org>
2019-05-13 10:39:59 +0800

    删除多余接口

--- base/src/main/java/com/trackray/base/store/VulnRepository.java
+++ base/src/main/java/com/trackray/base/store/VulnRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 @Repository
 public interface VulnRepository extends CrudRepository<VulnDTO, String> {
     List<VulnDTO> findAllByTaskMd5(String taskMd5);
+    List<VulnDTO> deleteAllByTaskMd5(String taskMd5);
+    List<VulnDTO> removeAllByTaskMd5(String taskMd5);
 }
